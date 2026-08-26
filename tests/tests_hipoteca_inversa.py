62fe841a99c2e98ddb773c275cd7fb4d9b43f4de
Jose-Dv <117058237+Jose-Dv@users.noreply.github.com>
2026-08-26 14:28:13 -0500

    Refactor tests for desembolso_mensual function

--- tests/tests_hipoteca_inversa.py
+++ tests/tests_hipoteca_inversa.py
@@ -1,159 +1,126 @@
 import unittest
 from src.model import logica_hipoteca_inversa
 
-
 class TestHipoteca(unittest.TestCase):
 
+    # Casos normales
     def test_normal_1(self):
-        # entradas
         valor_inmueble = 300_000_000
         porcentaje = 0.40
         tasa_mensual = 0.012
         plazo_meses = 120
 
-        # salidas
         cuota_esperada = 1892165.77
         abonos_esperado = 227059892.25
         intereses_esperado = 107059892.25
 
-        cuota, abonos, intereses = logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+        cuota, abonos, intereses = desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
 
         self.assertAlmostEqual(cuota, cuota_esperada, places=2)
         self.assertAlmostEqual(abonos, abonos_esperado, places=2)
         self.assertAlmostEqual(intereses, intereses_esperado, places=2)
 
     def test_normal_2(self):
-        # entradas
         valor_inmueble = 150_000_000
         porcentaje = 0.50
         tasa_mensual = 0.009
         plazo_meses = 60
 
-        # salidas
         cuota_esperada = 1623211.07
         abonos_esperado = 97392664.32
         intereses_esperado = 22392664.32
 
-        cuota, abonos, intereses = logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+        cuota, abonos, intereses = desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
 
         self.assertAlmostEqual(cuota, cuota_esperada, places=2)
         self.assertAlmostEqual(abonos, abonos_esperado, places=2)
         self.assertAlmostEqual(intereses, intereses_esperado, places=2)
 
     def test_normal_3(self):
-        # entradas
         valor_inmueble = 500_000_000
         porcentaje = 0.30
         tasa_mensual = 0.015
         plazo_meses = 48
 
-        # salidas
         cuota_esperada = 4406249.94
         abonos_esperado = 211499997.18
         intereses_esperado = 61499997.18
 
-        cuota, abonos, intereses = logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+        cuota, abonos, intereses = desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
 
         self.assertAlmostEqual(cuota, cuota_esperada, places=2)
         self.assertAlmostEqual(abonos, abonos_esperado, places=2)
         self.assertAlmostEqual(intereses, intereses_esperado, places=2)
 
-    def test_extraordinario_1_tasa_cero(self):
-        # entradas
+    # Casos extraordinarios
+    def test_extraordinario_tasa_cero(self):
         valor_inmueble = 200_000_000
         porcentaje = 0.50
         tasa_mensual = 0.0
         plazo_meses = 36
 
-        # salidas
         cuota_esperada = 2777777.78
         abonos_esperado = 100000000.00
         intereses_esperado = 0.00
 
-        cuota, abonos, intereses = logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+        cuota, abonos, intereses = desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
 
         self.assertAlmostEqual(cuota, cuota_esperada, places=2)
         self.assertAlmostEqual(abonos, abonos_esperado, places=2)
         self.assertAlmostEqual(intereses, intereses_esperado, places=2)
 
-    def test_extraordinario_2_unica_disposicion(self):
-        # entradas
+    def test_extraordinario_unica_disposicion(self):
         valor_inmueble = 100_000_000
         porcentaje = 1.00
         tasa_mensual = 0.024
         plazo_meses = 1
 
-        # salidas
         cuota_esperada = 102400000.00
         abonos_esperado = 102400000.00
         intereses_esperado = 2400000.00
 
-        cuota, abonos, intereses = logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+        cuota, abonos, intereses = desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
 
         self.assertAlmostEqual(cuota, cuota_esperada, places=2)
         self.assertAlmostEqual(abonos, abonos_esperado, places=2)
         self.assertAlmostEqual(intereses, intereses_esperado, places=2)
 
-    def test_extraordinario_3_plazo_240(self):
-        # entradas
+    def test_extraordinario_plazo_maximo(self):
         valor_inmueble = 250_000_000
         porcentaje = 0.40
         tasa_mensual = 0.018
         plazo_meses = 240
 
-        # salidas calculadas con la formula de anualidad
-        cuota_esperada = 1_825_226
-        abonos_esperado = 438_054_262
-        intereses_esperado = 338_054_262
+        cuota_esperada = 1825226
+        abonos_esperado = 438054262
+        intereses_esperado = 338054262
 
-        cuota, abonos, intereses = logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+        cuota, abonos, intereses = desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
 
         self.assertAlmostEqual(cuota, cuota_esperada, places=0)
         self.assertAlmostEqual(abonos, abonos_esperado, places=0)
         self.assertAlmostEqual(intereses, intereses_esperado, places=0)
 
-    def test_error_1_valor_inmueble_cero(self):
+    # Casos de error
+    def test_error_valor_inmueble_cero(self):
+        with self.assertRaises(ValorPropiedadCero):
+            desembolso_mensual(0, 0.40, 0.012, 60)
 
-        # entradas
-        valor_inmueble = 0
-        porcentaje = 0.40
-        tasa_mensual = 0.012
-        plazo_meses = 60
+    def test_error_tasa_usura(self):
+        with self.assertRaises(HipotecaUsura):
+            desembolso_mensual(200_000_000, 0.50, 0.05, 36)
 
-        with self.assertRaises(logica_hipoteca_inversa.ValorPropiedad0):
-            logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+    def test_error_plazo_cero(self):
+        with self.assertRaises(PlazoMenorIgualCero):
+            desembolso_mensual(150_000_000, 0.30, 0.012, 0)
 
-    def test_error_2_tasa_usura(self):
-        # entradas
-        valor_inmueble = 200_000_000
-        porcentaje = 0.50
-        tasa_mensual = 0.05
-        plazo_meses = 36
+    def test_error_plazo_negativo(self):
+        with self.assertRaises(PlazoMenorIgualCero):
+            desembolso_mensual(150_000_000, 0.30, 0.012, -12)
 
-        with self.assertRaises(logica_hipoteca_inversa.HipotecaUsura):
-            logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
-
-
-    def test_error_3_plazo_cero(self):
-        # entradas
-        valor_inmueble = 150_000_000
-        porcentaje = 0.30
-        tasa_mensual = 0.012
-        plazo_meses = 0
-
-        with self.assertRaises(logica_hipoteca_inversa.PlazoMenorIgual0):
-            logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
-
-
-    def test_error_4_plazo_negativo(self):
-        # entradas
-        valor_inmueble = 150_000_000
-        porcentaje = 0.30
-        tasa_mensual = 0.012
-        plazo_meses = 250
-
-        with self.assertRaises(logica_hipoteca_inversa.PlazoMayor240):
-            logica_hipoteca_inversa.desembolso_mensual(valor_inmueble, porcentaje, tasa_mensual, plazo_meses)
+    def test_error_plazo_mayor_240(self):
+        with self.assertRaises(PlazoMayorPermitido):
+            desembolso_mensual(150_000_000, 0.30, 0.012, 300)
 
 
 if __name__ == "__main__":
